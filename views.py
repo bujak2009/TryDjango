c6527c70c0108d5f1d5614dd73266e062c607bb1
bujak2009 <63642775+bujak2009@users.noreply.github.com>
2020-05-05 00:04:59 +0200

    Products model

--- views.py
+++ views.py
@@ -1,22 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-def home_view(request, *args, **kwargs):
-    return render(request, 'home.html', {})
-
-def contact_view(request, *args, **kwargs):
-    return render(request, 'contact.html', {'contact_mail': 'My e-mail adress is !@#$%', 'age': 'Only for 18+'})
-
-def about_view(request, *args, **kwargs):
-    my_context = {
-        'my_text': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-        'my_number': 123,
-        'my_list': [123, 455657, 'abc', 3.14, 'Anzej']
-    }
-    return render(request, 'about.html', my_context)
-
-def social_view(request, *args, **kwargs):
-    return HttpResponse('<h1>Social Page</h1')
-
 
 # Create your views here.
